ff32705f450945f3d7c639d22bb03458d466c9e9
Prateek Kumar Anand <prateekanand734@gmail.com>
2026-09-25 20:03:24 +0530

    fixing apk building

--- app/src/main/java/com/example/dailytracker/ui/components/QuickAddSheet.kt
+++ app/src/main/java/com/example/dailytracker/ui/components/QuickAddSheet.kt
@@ -20,7 +20,7 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
